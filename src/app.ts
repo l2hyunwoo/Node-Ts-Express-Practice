4c86a69d12a6335c418f2c0517b9fb2ee7055ca1
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-05-14 17:19:22 +0900

    tsoa config

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("Hello");
+import bodyParser from "body-parser";
+import express from "express";
+import { RegisterRoutes } from "../build/routes";
+
+export const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+RegisterRoutes(app);
